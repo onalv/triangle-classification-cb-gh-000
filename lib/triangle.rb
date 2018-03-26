f84d33da221e7d561fc08431ea549c39c829bd7b
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-26 15:40:22 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,9 +2,13 @@ class Triangle
 # :equilateral
 # :isosceles
 # :scalene
+  def initialize(side_1, side_2, side_3)
+    if side_1 == side_2 && side_2 == side_3
+      :equilateral
+    end
+  end
 
-  
   class TriangleError < StandardError
-    
+
   end
 end
