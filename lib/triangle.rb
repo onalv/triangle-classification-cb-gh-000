5bd7bebed179f1e56526dae285f2b0e14d1fdda3
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-26 15:41:40 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,6 +3,12 @@ class Triangle
 # :isosceles
 # :scalene
   def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def kind
     if side_1 == side_2 && side_2 == side_3
       :equilateral
     end
